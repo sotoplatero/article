11e6fe25c7ecc4c8455b2b1c0111a141e2c35712
Damian Soto <soto.platero@gmail.com>
2021-10-06 11:58:18 -0400

    update

--- api/article.js
+++ api/article.js
@@ -2,7 +2,25 @@ import Mercury from '@postlight/mercury-parser';
 
 module.exports = async (req, res) => {
   const { url } = req.query;
-  const article = await Mercury.parse(url)
-  console.log(article)
+  let article = {}
+
+  try {
+    const { 
+      title, 
+      author,
+      date_published: date,
+      lead_image_url: image,
+      word_count: words,
+      content,
+      excerpt,
+     } = await Mercury.parse(url)
+
+     article = { title, image, date, content, excerpt, author}
+
+  } catch(err) {
+    console.log(err)
+  }
+
+  res.setHeader('Cache-Control', 'public, immutable, no-transform, s-maxage=31536000, max-age=31536000');
   res.json(article);
 };
